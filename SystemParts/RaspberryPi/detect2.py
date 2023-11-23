a6e273a70335feb3e77a1cec7992b313f8c75a44
Alif024 <122778829+Alif024@users.noreply.github.com>
2023-11-23 23:08:10 +0700

    Update detect2.py

--- SystemParts/RaspberryPi/detect2.py
+++ SystemParts/RaspberryPi/detect2.py
@@ -38,6 +38,7 @@ def task1():
 def task2():
     global statusAir
     global schedule
+    peopleStatusPrevious = 0
     while True:
         #int schedule = digitalRead(schedulePin);
         # ตรวจคน 
